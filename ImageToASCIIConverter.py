eaaf4688551fd8f004e555c8d03326cc8673e02c
Dylan <dylantodd00@gmail.com>
2022-11-08 16:37:09 +0000

    Loaded images get transferred into ASCII format and exported into a .txt file

--- ImageToASCIIConverter.py
+++ ImageToASCIIConverter.py
@@ -1,2 +1,38 @@
 # This script will take an image and output a .txt file
 # with a simple ASCII representation of the image
+
+from PIL import Image
+
+img = Image.open("DogImage.jpg")
+img = img.resize((120, 70), Image.ANTIALIAS)
+pix = img.load()
+
+
+def main():
+    with open('ASCII_Art.txt', 'w') as f:
+        txtFileString = ""
+        for y in range(img.size[1]):
+            txtFileString += '\n'
+            for x in range(img.size[0]):
+                greyscaleValue = RGBToGreyscale(x, y)
+                txtFileString += ASCIIOutput(greyscaleValue)
+        
+        f.write(txtFileString)
+
+
+def RGBToGreyscale(x: int, y: int) -> int:
+    RGB = pix[x,y]
+    greyscaleValue = (RGB[0] + RGB[1] + RGB[2]) // 3
+    return greyscaleValue
+
+def ASCIIOutput(greyscaleValue: int) -> str:
+    if greyscaleValue > 200:
+        return '#'
+    elif greyscaleValue > 150:
+        return '<'
+    elif greyscaleValue > 100:
+        return '~'
+    elif greyscaleValue > 50:
+        return "'"
+    else:
+        return ' '
